fcca33b2325a384237b32c40b4dcf23b0de2d8a8
caprica-Six <mogwais@outlook.com>
2019-01-21 16:16:17 +0000

    add porduct props and click events on button to addItem

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,10 +5,35 @@ import { ProductConsumer } from '../context';
 
 export default class Product extends Component {
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
+
     return (
-      <div>
-        Product
-      </div>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+        <div className="img-container p-5" onClick={() => console.log('clicked')}>
+          <Link to="/details">
+            <img src={img} alt={title} className="card-img-top" />
+          </Link>
+          <button 
+            className="cart-btn" 
+            disabled={ inCart ? true : false }
+            onClick={() => {console.log('added to the Cart')}} 
+          >
+            {inCart ? (
+              <p className="text-capitalize mb-0" disabled>{" "} in inCart </p>
+              ) : ( 
+                <i className="fas fa-cart-plus" /> 
+              )
+            }
+          </button>
+        </div>
+
+        </div>
+      </ProductWrapper>
     )
   }
 }
+
+const ProductWrapper = styled.div`
+
+`
