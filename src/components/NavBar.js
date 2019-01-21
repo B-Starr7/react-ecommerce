dd9d65cd2d379c57629bd0ad421617f557ed244f
caprica-Six <mogwais@outlook.com>
2019-01-21 13:56:42 +0000

    add navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
 
 export default class NavBar extends Component {
     render() {
         return(
-            <div>Nav bar</div>
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to="/">
+                    <img src={logo} alt="Logo" className="navbar-brand" style={{width: '50px', height: '50px'}}
+                    />
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">Products</Link>
+                    </li>
+                </ul>
+                
+                <Link to="/cart" className="ml-auto">
+                    <button>
+                        <i className="fas fa-cart-plus">My Cart</i>
+                    </button>
+                </Link>
+            </nav>
         )
     }
 }
