c998d4a900079fea8d3b1ba30fa183b7ce2d73fa
caprica-Six <mogwais@outlook.com>
2019-01-21 13:39:39 +0000

    add react Reouter

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class Card extends Component {
+export default class Cart extends Component {
     render() {
         return (
             <div>
-                <h1>Card</h1>
+                <h1>Shopping Cart</h1>
             </div>
         )
     }
