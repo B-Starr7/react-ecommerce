6dfabac7b555357b32e1daed486579418f7db15f
caprica-Six <mogwais@outlook.com>
2019-01-21 14:45:58 +0000

    add Title component and props + render

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,24 @@
 import React, { Component, Fragment } from 'react'
 import Product from './Product';
+import Title from './Title';
+import { storeProducts } from '../data';
 
 export default class ProductList extends Component {
+    sate = { products: storeProducts };
+
     render() {
+        console.log(this.state.products);
+
         return(
             <Fragment>
-                <Product />
+                <div className="py-5">
+                    <div class="container">
+                        <div className="row">
+                            <Title name="our" title="products"/>
+                        </div>
+                    </div>
+                </div>
+                {/* <Product /> */}
             </Fragment>
         )
     }
